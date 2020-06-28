cbb3069254dbfa01f549f8f7c4bafeb63ba517f1
OscarDRT <oscarnetworkingpro@gmail.com>
2020-06-28 15:07:15 -0500

    new modules and configurations

--- Food_Trucks/src/index.js
+++ Food_Trucks/src/index.js
@@ -1,26 +1,49 @@
-// Server is set up with express
-
 const express = require('express');
-const engine = require('ejs-mate')
-const path = require('path')
-
-//  initialization 
 const app = express();
 
+const path = require('path');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+const { url } = require('./config/databases.js');
+
+mongoose.connect(url, {
+	useMongoClient: true
+});
+
+require('./config/passport')(passport);
 
 // settings
-app.engine('ejs', engine);
-app.set('port', 8080);
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '/views'))
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// middlewares
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+// required for passport
+app.use(session({
+	secret: 'faztwebtutorialexample',
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 // routes
-app.use(require('./routes'));
+require('./app/routes.js')(app, passport);
 
 // static files
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-// starting the server
+// start the server
 app.listen(app.get('port'), () => {
-    console.log(`Server in port: ${app.get('port')}`)
+	console.log('server on port ', app.get('port'));
 });
