8ad63767bd632a122c26eb7b6787c7a58f8f391b
OscarDRT <oscarnetworkingpro@gmail.com>
2020-06-27 19:31:59 -0500

    file to set the paths of my application

--- Food_Trucks/src/routes/index.js
+++ Food_Trucks/src/routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router();
 
+//main path
 router.get('/', (req, res) => {
     res.render('index');
 });
+
+/* 
+if new routes are required add them here 
+*/
 
 module.exports = router;
